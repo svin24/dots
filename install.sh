3ca40ea11500a70d892c2c752c13a9735cca5796
svin24 <svin.main.24@gmail.com>
2025-09-16 22:37:19 +0300

    update

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Installer script: Copies all folders from the current directory (dots repo) to ~/.config
 # Run this from the root of your dots project directory
 # Warning: This will overwrite existing folders in ~/.config with the same name
 
-set -e  # Exit on error
+set -e # Exit on error
 
 TARGET_DIR="$HOME/.config"
 
 if [ ! -d "$TARGET_DIR" ]; then
-    mkdir -p "$TARGET_DIR"
+  mkdir -p "$TARGET_DIR"
 fi
 
 for folder in */; do
-    if [ -d "$folder" ]; then
-        folder_name="${folder%/}"  # Remove trailing slash
-        echo "Installing $folder_name to $TARGET_DIR/$folder_name"
-        cp -r "$folder" "$TARGET_DIR/"
-    fi
+  if [ -d "$folder" ]; then
+    folder_name="${folder%/}" # Remove trailing slash
+    echo "Installing $folder_name to $TARGET_DIR/$folder_name"
+    cp -r "$folder" "$TARGET_DIR/"
+  fi
 done
 
 echo "Installation complete."
--- update.sh
+++ update.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Updater script: Copies back folders from ~/.config to the current directory (dots repo)
 # Only updates folders that exist in the dots repo
 # Run this from the root of your dots project directory
 # Warning: This will overwrite files in the repo with versions from ~/.config
 
-set -e  # Exit on error
+set -e # Exit on error
 
 SOURCE_DIR="$HOME/.config"
 
 for folder in */; do
-    if [ -d "$folder" ]; then
-        folder_name="${folder%/}"  # Remove trailing slash
-        if [ -d "$SOURCE_DIR/$folder_name" ]; then
-            echo "Updating $folder_name from $SOURCE_DIR/$folder_name"
-            cp -r "$SOURCE_DIR/$folder_name" .
-        else
-            echo "Skipping $folder_name: Not found in $SOURCE_DIR"
-        fi
+  if [ -d "$folder" ]; then
+    folder_name="${folder%/}" # Remove trailing slash
+    if [ -d "$SOURCE_DIR/$folder_name" ]; then
+      echo "Updating $folder_name from $SOURCE_DIR/$folder_name"
+      cp -r "$SOURCE_DIR/$folder_name" .
+    else
+      echo "Skipping $folder_name: Not found in $SOURCE_DIR"
     fi
+  fi
 done
 
 echo "Update complete."
